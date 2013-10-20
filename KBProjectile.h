187266cacc1b4b54de50915adf98228dfa28cd3b
Cristian Pereyra <cristian@redmintlabs.com>
2013-10-20 13:27:41 -0300

    creates projectile stub

---
+++ KBProjectile.h
@@ -0,0 +1,13 @@
+//
+//  KBProjectile.h
+//  killbills
+//
+//  Created by Cristian Pereyra on 20/10/13.
+//  Copyright (c) 2013 Cristian Pereyra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KBProjectile : NSObject
+
+@end
---
+++ KBProjectile.m
@@ -0,0 +1,13 @@
+//
+//  KBProjectile.m
+//  killbills
+//
+//  Created by Cristian Pereyra on 20/10/13.
+//  Copyright (c) 2013 Cristian Pereyra. All rights reserved.
+//
+
+#import "KBProjectile.h"
+
+@implementation KBProjectile
+
+@end
