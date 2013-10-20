b28bb894af128b29ded663c233daca251685e6aa
Cristian Pereyra <cristian@redmintlabs.com>
2013-10-20 02:11:49 -0300

    add fullscreen game

--- killbills/KBAppDelegate.m
+++ killbills/KBAppDelegate.m
@@ -56,7 +56,8 @@
 {
 	// Create the main window
 	window_ = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-	
+
+    [[UIApplication sharedApplication] setStatusBarHidden:YES];
 	
 	// CCGLView creation
 	// viewWithFrame: size of the OpenGL view. For full screen use [_window bounds]
