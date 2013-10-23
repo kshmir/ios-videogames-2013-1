a9b7e2f3ec667a1a2b9849bb3c4bed4da6f17647
Cristian Pereyra <cristian@redmintlabs.com>
2013-10-23 02:11:38 -0300

    splits shared specs, this is awesome!

--- killbillsTests/KBSharedSpecs.m
+++ killbillsTests/KBSharedSpecs.m
@@ -1,1 +1,35 @@
 #import "SpecHelper.m"
+
+#import "KBGameMovement.h"
+#import "KBMonster.h"
+#import "KBProjectile.h"
+
+SharedExamplesBegin(KBMovingObject)
+
+sharedExamplesFor(@"a moving object", ^(NSDictionary * data) {
+    it(@"respects the protocol", ^{
+        id<KBMovingObject> object = [data objectForKey:@"movingObject"];
+        expect(object).to.conformTo(@protocol(KBMovingObject));
+    });
+    it(@"should have a movement object", ^{
+        id<KBMovingObject> object = [data objectForKey:@"movingObject"];
+        expect([object movement]).notTo.beNil();
+    });
+});
+
+SharedExamplesEnd
+
+SharedExamplesBegin(KBGameObject)
+
+sharedExamplesFor(@"a game object", ^(NSDictionary * data) {
+    it(@"respects the protocol", ^{
+        id<KBGameObject> object = [data objectForKey:@"gameObject"];
+        expect(object).to.conformTo(@protocol(KBGameObject));
+    });
+    it(@"should have a sprite object", ^{
+        id<KBGameObject> object = [data objectForKey:@"gameObject"];
+        expect([object sprite]).notTo.beNil();
+    });
+});
+
+SharedExamplesEnd
