d307b0e3c4e438afe751d04a06c700ae2043d6bb
Cristian Pereyra <cristian@redmintlabs.com>
2013-10-27 13:32:02 -0300

    agrega boton invisible para cheats

--- killbills/KBMenu.m
+++ killbills/KBMenu.m
@@ -44,11 +44,16 @@
     KBMenu * menu = [[KBMenu alloc] init];
     
     CCLabelTTF *lbl_Home = [CCLabelTTF labelWithString:@"Resume" fontName:@"verdana" fontSize:25];
-   
     [lbl_Home setColor:ccc3(0,0,0)];
     
+    
+    CCLabelTTF *lbl_Cheat = [CCLabelTTF labelWithString:@"Cheat" fontName:@"verdana" fontSize:25];
+    [lbl_Cheat setColor:ccc3(255,255,255)];
+    [lbl_Cheat setOpacity:0];
+    
     CCMenuItemLabel *lbl = [CCMenuItemLabel itemWithLabel:lbl_Home target:menu selector:@selector(resumeClicked)];
-    CCMenu *mnu = [CCMenu menuWithItems:lbl,nil];
+    CCMenuItemLabel *lbl2 = [CCMenuItemLabel itemWithLabel:lbl_Cheat target:menu selector:@selector(resumeClicked)];
+    CCMenu *mnu = [CCMenu menuWithItems:lbl,lbl2,nil];
     CGSize winSize = [[CCDirector sharedDirector] winSize];
     [mnu setPosition:ccp(winSize.width/2,winSize.height/2)];
     [mnu alignItemsVertically];
