6a9ef76f6c3dd1e32034a3ca5c25a1fb3c69b3f5
Cristian Pereyra <cristian@redmintlabs.com>
2013-11-21 19:24:58 -0300

    agrega todo

---
+++ Pods/Pods-spectaTests-Expecta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_spectaTests_Expecta : NSObject
+@end
+@implementation PodsDummy_Pods_spectaTests_Expecta
+@end
---
+++ Pods/Pods-spectaTests-OCMock-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_spectaTests_OCMock : NSObject
+@end
+@implementation PodsDummy_Pods_spectaTests_OCMock
+@end
